9adc58953d6df3cf4e8ae69ba72124ec239f24df
JanElmirov <99734651+JanElmirov@users.noreply.github.com>
2022-03-02 18:33:40 +0600

    Add files via upload

---
+++ function hw/main.js
@@ -0,0 +1,36 @@
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+// let main = arr[2]
+// function ahahah(){ 
+//  for(let i = 0; i<arr.length; i++) {
+// 	 if (arr[i] === main) {
+// 		 arr.splice( i, 1)
+// 		 main += 3
+// 	 }
+//  }
+// }
+// ahahah()
+//  console.log(arr);
+//-------------------------------------------- \\
+// function test(){
+// 	 let arr = []
+// 	 for(let i=0; i<1; i++){
+// 		let i = prompt('Ты кто такой')
+// 		if ( i == 'Человек') {
+// 			arr.push(i)
+// 		} 
+// 		i = prompt('Сколько тебе лет') 
+// 		if (i == '18') {
+// 		arr.push(i)
+// 	}
+// 		i = prompt('Где учищься')
+// 		if (i == 'В универе') {
+// 		arr.push(i)
+// 	}
+// 	i = prompt('Есть машина?')
+// 	if ( i == 'нет') {
+// 		arr.push(i)
+// 	}
+//  }
+// console.log(arr);
+// }
+// test()
